1d8393d1f6902f6bb234f282c9eed3099e638fdb
Tom Bortels <bortels@gmail.com>
2014-02-21 23:47:19 -0800

    Create bortels.us.lua
    basic config

---
+++ bortels.us.lua
@@ -0,0 +1,12 @@
+-- File: bortels.us.lua
+-- Zone: bortels.us
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "bortels.us"
+
+-- A records
+a(_a, "162.195.210.119")
+
+-- CNAME records
+cname("www", _a)
+
